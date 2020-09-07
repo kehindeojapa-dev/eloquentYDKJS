355632fee886f02c4e5708c72e1b31b891d1b42e
Kehinde <adaeza.dev@gmail.com>
2020-09-07 14:59:06 +0100

    Initial commit for eloquentYDKJS code samples and practices, also included chapter1 code samples

---
+++ eqJS/Chapter1/chapter1.js
@@ -0,0 +1,88 @@
+    
+    // Values, Types, and Operators
+
+
+//  Number value
+
+    //Operators : +, -, *, /, % 
+
+    // 0 / 0
+    // Infinity - Infinity
+    // 10 % 3
+
+
+//  String value
+
+    // Escape characters: \n,
+
+    // "This is not a new line'\n'dont appear on a new line"
+    // "This is not a new line\"\\n\"ndont appear on a new line"
+    // "This is not a new line"\n"ndont appear on a new line"
+    // "This is not a new line\"\\n\"ndont appear on a new line"
+    // "This is not a new line"\n"ndont appear on a new line"
+    // "This is not a escape caracter \"\\n" wdsd"
+    // VM609:1 Uncaught SyntaxError: Unexpected identifier
+    // "This is not a escape caracter \"\\n\" wdsd"
+    // "This is not a escape caracter "\n" wdsd"
+
+    // Unary Operator : typeof, minus symbol.
+    // console.log (typeof 4.5);
+    // console.log (typeof "4.5");
+
+
+//  Boolean value
+
+    //two possible outcomes: true/false
+    // console.log(3 > 2);
+    // console.log(3 >= 3);
+    // console.log(3 == 3);
+    // console.log(3 != 3);
+    
+    // String comparison
+    // console.log("Abd" > "Abc");
+    // console.log("Abc@" > "Abc!");
+    // console.log("Abd!" > "Abc@");
+
+
+    //  Logical Operators
+
+    // console.log(true && true);
+    // console.log(true && false);
+    // console.log(true || true);
+    // console.log(false || true);
+    // console.log(false || false);
+    // console.log(!(false))
+    // console.log(!(true || false));
+    // console.log(1+1==2 && 10*10>50);
+    // console.log(1+1==2 && !10*10>50);
+
+
+    //  Ternary Operator
+
+    // console.log(true ? 10 : 9);
+    // console.log(false ? 10 : 9);
+    // console.log(true ? 0 : 1);
+    // console.log(true ? 1 : 0);
+
+
+//  Type Coercion
+
+    // console.log(0 == false) //-> true
+    // console.log(1 == true) //-> true
+    // console.log(0 === false) //-> false
+    // console.log(1 === true) //-> false
+    // console.log("5" == 5)   //-> true
+    // console.log("5" === 5)   //-> false
+    
+
+// Short Circuit Evaluation
+
+    // console.log(null || "user")     //-> "user"
+    // console.log("agnes" || "user")  //-> "agnes"
+    // console.log(null && "user")     //-> null
+    // console.log("agnes" && "user")  //-> "user"
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,6 @@
+# EloquentJS + YDKJS
+
+This repo contains code samples and practice from the books: Eloquent Javascript and you don't know JS series
+
+eqJS: contain resources from eloquent javascript book.
+ydkjs: contain resources from you don't know js series book.
